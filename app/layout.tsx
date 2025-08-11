65aa32ddef2941ef81ef9105d6d396bc25e5b6ff
08abhinav <abhinavnegi101@gmail.com>
2025-08-11 18:50:01 +0530

    updated: added queryprovider and sheetprovider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import { type Metadata } from 'next'
-import {
-  ClerkProvider,
-} from '@clerk/nextjs'
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+import { type Metadata } from 'next'
+import { ClerkProvider,} from '@clerk/nextjs'
+import { Geist, Geist_Mono } from 'next/font/google'
+import { QueryProvider } from '@/providers/query-provider'
+import { SheetProvider } from '@/providers/sheet-providers'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -29,7 +29,10 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          {children}
+          <QueryProvider>
+            <SheetProvider/>
+            {children}
+          </QueryProvider>
         </body>
       </html>
     </ClerkProvider>
