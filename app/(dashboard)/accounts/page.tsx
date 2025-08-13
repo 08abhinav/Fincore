f39b3a3a3dfa5585fd249e4d37b50d1243100ce0
08abhinav <abhinavnegi101@gmail.com>
2025-08-13 12:38:01 +0530

    feat: created a new page for accounts route to show all accouns info

---
+++ app/(dashboard)/accounts/page.tsx
@@ -0,0 +1,79 @@
+"use client"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card"
+import { Loader2, Plus } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { Skeleton } from "@/components/ui/skeleton"
+
+import { columns } from "./columns"
+import { DataTable } from "./data-table"
+
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account"
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts"
+import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete"
+
+
+const AccountsPage = () => {
+  const newAccount = useNewAccount();
+  const accountsQuery = useGetAccounts();
+  const deleteAccount = useBulkDeleteAccounts();
+  const accounts = accountsQuery.data || [];
+
+  const isDisabled = accountsQuery.isLoading || deleteAccount.isPending;
+
+  if(accountsQuery.isLoading){
+    return(
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader>
+            <Skeleton className="h-8 w-48"/>
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px] w-full flex items-center justify-center">
+              <Loader2 className="size-8 text-slate-400 animate-spin"/>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    )
+  }
+
+  return (
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="flex flex-col gap-y-2 lg:flex-row lg:items-center lg:justify-between lg:gap-y-0">
+          <CardTitle className="text-xl font-semibold line-clamp-1">
+            Accounts Page
+          </CardTitle>
+          <Button
+            onClick={newAccount.onOpen}
+            size="sm"
+            className="flex items-center">
+            <Plus className="size-4 mr-2" />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable 
+            filterKey="name"
+            columns={columns} 
+            data={accounts}
+            onDelete={(row)=>{
+              const ids = row.map((r) => r.original.id);
+              deleteAccount.mutate({ids})
+            }}
+            disabled={isDisabled} 
+          />
+        </CardContent>
+      </Card>
+    </div>
+
+  )
+}
+
+export default AccountsPage
