3f06fc8fc3b03c887e04d0bd817da7a9b1e6a04c
08abhinav <abhinavnegi101@gmail.com>
2025-08-14 14:09:56 +0530

    update: added action for deleting the transaction

--- app/(dashboard)/accounts/actions.tsx
+++ app/(dashboard)/accounts/actions.tsx
@@ -9,8 +9,10 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator
 } from "@/components/ui/dropdown-menu";
+import { useDeleteAccount } from "@/features/accounts/api/use-delete-account";
 import { useOpenAccount } from "@/features/accounts/hooks/use-open-account";
-import { Edit, MoreHorizontal } from "lucide-react";
+import { useConfirm } from "@/hooks/use-confirm";
+import { Edit, MoreHorizontal, Trash } from "lucide-react";
 
 type Props = {
   id: string;
@@ -18,31 +20,53 @@ type Props = {
 
 export const Actions = ({ id }: Props) => {
     const {onOpen} = useOpenAccount();
+    const deleteMutation = useDeleteAccount(id);
+    const [ConfirmDialog, confirm] = useConfirm(
+      "Are you sure?",
+      "You want to delete this transaction."
+    )
+    const handleDelete = async ()=>{
+      const ok = await confirm();
+      if(ok){
+        deleteMutation.mutate()
+      }
+    }
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="h-8 w-8 p-0 hover:bg-muted"
-        >
-          <MoreHorizontal className="h-4 w-4" />
-        </Button>
-      </DropdownMenuTrigger>
+    <>
+      <ConfirmDialog/>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            size="icon"
+            className="h-8 w-8 p-0 hover:bg-muted"
+          >
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
 
-      <DropdownMenuContent
-        align="end"
-        className="w-40 rounded-lg border border-border bg-popover p-1 shadow-md"
-      >
-        <DropdownMenuItem
-          disabled={false}
-          onClick={() => onOpen(id)}
-          className="flex items-center gap-2 px-2 py-1.5 text-sm cursor-pointer rounded-md hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+        <DropdownMenuContent
+          align="end"
+          className="w-40 rounded-lg border border-border bg-popover p-1 shadow-md"
         >
-          <Edit className="h-4 w-4 text-muted-foreground" />
-          Edit
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <DropdownMenuItem
+            disabled={deleteMutation.isPending}
+            onClick={() => onOpen(id)}
+            className="flex items-center gap-2 px-2 py-1.5 text-sm cursor-pointer rounded-md hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+          >
+            <Edit className="h-4 w-4 text-muted-foreground" />
+            Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            disabled={deleteMutation.isPending}
+            onClick={handleDelete}
+            className="flex items-center gap-2 px-2 py-1.5 text-sm cursor-pointer rounded-md hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+          >
+            <Trash className="h-4 w-4 text-muted-foreground" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   );
 };
