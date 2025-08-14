fbecc9d162aadb683c6c8839fecee538c130603a
08abhinav <abhinavnegi101@gmail.com>
2025-08-14 14:09:17 +0530

    udpate: added the delete hooks and cofirmation functionality

--- features/accounts/components/edit-account-sheet.tsx
+++ features/accounts/components/edit-account-sheet.tsx
@@ -5,12 +5,15 @@ import { Sheet,
     SheetDescription
 } from "@/components/ui/sheet";
 import z from "zod";
-import { insertAccountSchema } from "@/db/schema";
-import { AccountForm } from "@/features/accounts/components/account-form";
-import { useOpenAccount } from "../hooks/use-open-account";
-import { useGetAccount } from "../api/use-get-account";
 import { Loader2 } from "lucide-react";
+import { insertAccountSchema } from "@/db/schema";
+import { useOpenAccount } from "../hooks/use-open-account";
+
+import { useGetAccount } from "../api/use-get-account";
 import { useEditAccount } from "../api/use-edit-account";
+import { AccountForm } from "@/features/accounts/components/account-form";
+import { useDeleteAccount } from "../api/use-delete-account";
+import { useConfirm } from "@/hooks/use-confirm";
 
 const formSchema = insertAccountSchema.pick({
     name: true,
@@ -20,13 +23,17 @@ type FormValues = z.input<typeof formSchema>
 
 export const EditAccountSheet = ()=>{
     const {isOpen, onClose, id} = useOpenAccount();
+    const [ConfirmDialog, confirm] = useConfirm(
+        "Are you sure?",
+        "You want to delete this transaction."
+    )
 
     const accountQuery = useGetAccount(id);
     const editMutation = useEditAccount(id);
+    const deleteMutation = useDeleteAccount(id);
 
     const isLoading = accountQuery.isLoading;
-
-    const isPending = editMutation.isPending
+    const isPending = editMutation.isPending || deleteMutation.isPending
 
     const onSubmit = (values: FormValues)=>{
         editMutation.mutate(values, {
@@ -36,33 +43,50 @@ export const EditAccountSheet = ()=>{
         })
     }
 
+    const onDelete = async ()=>{
+        const ok = await confirm();
+
+        if(ok){
+            deleteMutation.mutate(undefined, {
+                onSuccess: ()=>{
+                    onClose();
+                }
+            })
+        }
+    }
+
     const defaultValues = accountQuery.data ? {
         name: accountQuery.data.name
     }:{name: "",}
     return(
-        <Sheet open={isOpen} onOpenChange={onClose}>
-            <SheetContent className="space-y-4 p-6">
-                <SheetHeader className="space-y-2">
-                    <SheetTitle className="text-xl font-semibold tracking-tight">
-                        Edit Account
-                    </SheetTitle>
-                    <SheetDescription  className="text-sm text-muted-foreground">
-                        Edit an existing account.
-                    </SheetDescription>
-                </SheetHeader>
-                {isLoading ? (
-                    <div className="absolute inset-0 flex items-center justify-center">
-                        <Loader2 className="h-4 w-4 text-muted-foreground animate-spin"/>
-                    </div>
-                    ):(
-                        <AccountForm
-                        id={id} 
-                        defaultValues={defaultValues} 
-                        onSubmit={onSubmit} 
-                        disabled={isPending}/>
-                    )
-                }
-            </SheetContent>
-        </Sheet>
+        <>
+            <ConfirmDialog/>
+            <Sheet open={isOpen} onOpenChange={onClose}>
+                <SheetContent className="space-y-4 p-6">
+                    <SheetHeader className="space-y-2">
+                        <SheetTitle className="text-xl font-semibold tracking-tight">
+                            Edit Account
+                        </SheetTitle>
+                        <SheetDescription  className="text-sm text-muted-foreground">
+                            Edit an existing account.
+                        </SheetDescription>
+                    </SheetHeader>
+                    {isLoading ? (
+                        <div className="absolute inset-0 flex items-center justify-center">
+                            <Loader2 className="h-4 w-4 text-muted-foreground animate-spin"/>
+                        </div>
+                        ):(
+                            <AccountForm
+                                id={id} 
+                                defaultValues={defaultValues} 
+                                onSubmit={onSubmit} 
+                                disabled={isPending}
+                                onDelete={onDelete}
+                            />
+                        )
+                    }
+                </SheetContent>
+            </Sheet>
+        </>
     )
 }
