385a12356f7cc37b719da16cefca304d3ebb467b
08abhinav <abhinavnegi101@gmail.com>
2025-08-18 16:34:37 +0530

    feat: created action for categories field

---
+++ app/(dashboard)/categories/actions.tsx
@@ -0,0 +1,72 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuLabel,
+  DropdownMenuSeparator
+} from "@/components/ui/dropdown-menu";
+import { useDeleteCategory } from "@/features/categories/api/use-delete-category";
+import { useOpenCategory } from "@/features/categories/hooks/use-open-category";
+import { useConfirm } from "@/hooks/use-confirm";
+import { Edit, MoreHorizontal, Trash } from "lucide-react";
+
+type Props = {
+  id: string;
+};
+
+export const Actions = ({ id }: Props) => {
+    const {onOpen} = useOpenCategory();
+    const deleteMutation = useDeleteCategory(id);
+    const [ConfirmDialog, confirm] = useConfirm(
+      "Are you sure?",
+      "You want to delete this category."
+    )
+    const handleDelete = async ()=>{
+      const ok = await confirm();
+      if(ok){
+        deleteMutation.mutate()
+      }
+    }
+  return (
+    <>
+      <ConfirmDialog/>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            size="icon"
+            className="h-8 w-8 p-0 hover:bg-muted"
+          >
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent
+          align="end"
+          className="w-40 rounded-lg border border-border bg-popover p-1 shadow-md"
+        >
+          <DropdownMenuItem
+            disabled={deleteMutation.isPending}
+            onClick={() => onOpen(id)}
+            className="flex items-center gap-2 px-2 py-1.5 text-sm cursor-pointer rounded-md hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+          >
+            <Edit className="h-4 w-4 text-muted-foreground" />
+            Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            disabled={deleteMutation.isPending}
+            onClick={handleDelete}
+            className="flex items-center gap-2 px-2 py-1.5 text-sm cursor-pointer rounded-md hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+          >
+            <Trash className="h-4 w-4 text-muted-foreground" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
