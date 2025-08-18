71b6940b20a122e80d0905eb35dc0c8b578b65d3
08abhinav <abhinavnegi101@gmail.com>
2025-08-18 16:38:01 +0530

    update: added a new table for category

--- db/schema.ts
+++ db/schema.ts
@@ -6,5 +6,11 @@ export const accounts = pgTable("accounts", {
     name: text("name").notNull(),
     userId: text("user_id").notNull(),
 });
-
 export const insertAccountSchema = createInsertSchema(accounts);
+
+export const categories = pgTable("categories", {
+    id: text("id").primaryKey(),
+    name: text("name").notNull(),
+    userId: text("user_id").notNull(),
+});
+export const insertCategorySchema = createInsertSchema(categories);
