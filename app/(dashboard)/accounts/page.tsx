93979072dfb2851e253146719e7ceb0ae415e4eb
08abhinav <abhinavnegi101@gmail.com>
2025-08-18 16:32:23 +0530

    update: changed the name of file

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -15,7 +15,7 @@ import { DataTable } from "./data-table"
 
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account"
 import { useGetAccounts } from "@/features/accounts/api/use-get-accounts"
-import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete"
+import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete-accounts"
 
 
 const AccountsPage = () => {
