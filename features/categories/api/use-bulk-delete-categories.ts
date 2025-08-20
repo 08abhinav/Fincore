0de3d63b5f48da2460720fe2dd83485b513c8e23
08abhinav <abhinavnegi101@gmail.com>
2025-08-20 20:40:42 +0530

    feat: new hook for bulk delete of categories

---
+++ features/categories/api/use-bulk-delete-categories.ts
@@ -0,0 +1,30 @@
+import {client} from "@/lib/hono"
+import { toast } from "sonner";
+import { InferRequestType, InferResponseType } from "hono";
+import {useMutation, useQueryClient} from "@tanstack/react-query"
+
+type ResponseType = InferResponseType<typeof client.api.categories["bulk-delete"]["$post"]>;
+type RequestType = InferRequestType<typeof client.api.categories["bulk-delete"]["$post"]>["json"]
+
+export const useBulkDeleteCategory = ()=>{
+    const queryClient = useQueryClient();
+    const mutation = useMutation<
+    ResponseType,
+    Error,
+    RequestType
+    >({
+        mutationFn: async (json)=>{
+            const res = await client.api.categories["bulk-delete"]["$post"]({json})
+            return await res.json();
+        },
+        onSuccess: () =>{
+            toast.success("Categories deleted ")
+            queryClient.invalidateQueries({queryKey:["categories"]})
+        },
+        onError: () =>{
+            toast.error("Failed to delete categories")
+        }
+    })
+    return mutation;
+}
+ 
