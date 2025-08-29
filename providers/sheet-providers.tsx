77d64acec1e605aebf3c03372e1c5fac6c0abae1
08abhinav <abhinavnegi101@gmail.com>
2025-08-29 22:25:58 +0530

    update: added edittransactionsheet

--- providers/sheet-providers.tsx
+++ providers/sheet-providers.tsx
@@ -7,6 +7,7 @@ import {NewCategorySheet} from "@/features/categories/components/new-category-sh
 import {EditCategorySheet} from "@/features/categories/components/edit-category-sheet";
 
 import { NewTransactionSheet } from "@/features/transactions/components/new-transaction-sheet";
+import { EditTransactionSheet } from "@/features/transactions/components/edit-transaction-sheet";
 
 export const SheetProvider = ()=>{
     const isMounted = useMountedState();
@@ -24,6 +25,7 @@ export const SheetProvider = ()=>{
 
             {/* Sheet for creating transaction */}
             <NewTransactionSheet/>
+            <EditTransactionSheet/>
         </>
     )
 }
