dc43bda156901f589ca3661d30da924452a1cfc5
08abhinav <abhinavnegi101@gmail.com>
2025-08-29 22:24:01 +0530

    update: changed the action according to the transactions

--- app/(dashboard)/transactions/actions.tsx
+++ app/(dashboard)/transactions/actions.tsx
@@ -9,8 +9,8 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator
 } from "@/components/ui/dropdown-menu";
-import { useDeleteAccount } from "@/features/accounts/api/use-delete-account";
-import { useOpenAccount } from "@/features/accounts/hooks/use-open-account";
+import { useDeleteTransaction } from "@/features/transactions/api/use-delete-transaction";
+import { useOpenTransaction } from "@/features/transactions/hooks/use-open-transaction";
 import { useConfirm } from "@/hooks/use-confirm";
 import { Edit, MoreHorizontal, Trash } from "lucide-react";
 
@@ -19,11 +19,11 @@ type Props = {
 };
 
 export const Actions = ({ id }: Props) => {
-    const {onOpen} = useOpenAccount();
-    const deleteMutation = useDeleteAccount(id);
+    const {onOpen} = useOpenTransaction();
+    const deleteMutation = useDeleteTransaction(id);
     const [ConfirmDialog, confirm] = useConfirm(
       "Are you sure?",
-      "You want to delete this account."
+      "You want to delete this transaction."
     )
     const handleDelete = async ()=>{
       const ok = await confirm();
