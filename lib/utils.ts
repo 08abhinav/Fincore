413462c77da90af1e7ee666c9caf97d33c89b551
08abhinav <abhinavnegi101@gmail.com>
2025-08-30 17:32:15 +0530

    update: added 2 new functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,13 +1,10 @@
 import { clsx, type ClassValue } from "clsx"
+import { eachDayOfInterval, isSameDay } from "date-fns";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-// export function converAmountToMili(amount: number){
-//   return Math.round(amount * 1000);
-// }
 
 export function converAmountFromMili(amount: number){
   return amount / 1000;
@@ -19,4 +16,48 @@ export function formatCurrency(amount: number){
     style: "currency",
     currency: "INR"})
     .format(amount)
+}
+
+export function calcuatePercentageChange(
+  current: number,
+  previous: number
+){
+  if(previous == 0){
+    return previous === current ? 0 : 100;
+  }
+  return ((current - previous) / previous) * 100;
+}
+
+
+export function fillMissingDays(
+  activeDays: {
+    date: Date;
+    income: number;
+    expenses: number;
+  }[],
+  startDate: Date,
+  endDate: Date,
+){
+  if(activeDays.length === 0){
+    return [];
+  }
+  const allDays = eachDayOfInterval({
+    start: startDate,
+    end: endDate,
+  })
+
+  const transactionsByDate = allDays.map((day) => {
+    const found = activeDays.find((d)=> isSameDay(d.date, day));
+    if(found){
+      return found;
+    }else{
+      return {
+        date: day,
+        income: 0,
+        expenses: 0,
+      }
+    }
+  })
+
+  return transactionsByDate
 }
