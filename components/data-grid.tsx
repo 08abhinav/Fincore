74f6b1d9c8a2d3462e766ae8ac89fcb4d7986ee0
08abhinav <abhinavnegi101@gmail.com>
2025-09-01 20:03:06 +0530

    update: adding income and expense card and also added loading skeleton for them

--- components/data-grid.tsx
+++ components/data-grid.tsx
@@ -3,17 +3,26 @@ import { useGetSummary } from '@/features/summary/use-get-summary';
 import { formatDateRange } from '@/lib/utils';
 import { useSearchParams } from 'next/navigation'
 import { FaPiggyBank } from 'react-icons/fa'
+import { FaArrowTrendUp, FaArrowTrendDown } from 'react-icons/fa6'
 import React from 'react'
-import { DataCard } from './data-card';
+import { DataCard, DataCardLoading } from './data-card';
 
 export const DataGrid = () => {
-    const {data} = useGetSummary();
+    const {data, isLoading} = useGetSummary();
     const params = useSearchParams();
     const to = params.get("to") || undefined;
     const from = params.get("from") || undefined;
-
     const dataRangeLabel = formatDateRange({to, from});
 
+    if(isLoading){
+      return(
+       <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8'>
+        <DataCardLoading/>
+        <DataCardLoading/>
+        <DataCardLoading/>
+       </div> 
+      )
+    }
   return (
     <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8'>
         <DataCard
@@ -21,6 +30,22 @@ export const DataGrid = () => {
             value={data?.remainingAmount}
             percentageChange={data?.remainingChange}
             icon={FaPiggyBank}
+            variant="default"
+            dateRange={dataRangeLabel}
+        />
+        <DataCard
+            title="Income"
+            value={data?.incomeAmount}
+            percentageChange={data?.incomeChange}
+            icon={FaArrowTrendUp}
+            variant="default"
+            dateRange={dataRangeLabel}
+        />
+        <DataCard
+            title="Expenses"
+            value={data?.expensesAmount}
+            percentageChange={data?.expensesChange}
+            icon={FaArrowTrendDown}
             variant="default"
             dateRange={dataRangeLabel}
         />
